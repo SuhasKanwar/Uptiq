c321498b47a8004fafcc1d8a03caef9b066b1a10
Suhas Kanwar <suhas.kanwar@gmail.com>
2026-03-01 00:45:15 +0530

    poem init

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use poem::{Route, Server, get, handler, listener::TcpListener, post, web::Path};
+
+#[handler]
+fn index() -> String {
+    format!("Server is running successfully!!!")
+}
+
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("Website ID: {}", website_id)
+}
+
+#[handler]
+fn create_website() -> String {
+    format!("Creating a new website entry...")
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let app = Route::new()
+        .at("/", get(index))
+        .at("/website/:website_id", get(get_website))
+        .at("/website", post(create_website));
+    
+    Server::new(TcpListener::bind("0.0.0.0:9000"))
+        .run(app)
+        .await
 }
