f036c03f0efa34ba8a8aafc5b4df9603ce724ddf
SuhasKanwar <suhas.kanwar@gmail.com>
2026-05-24 20:03:50 +0530

    added frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Uptiq",
+  description: "Uptiq is a scalable website monitoring platform that continuously checks the availability and performance of user-added services. Built with a distributed architecture using pollers, queues, and workers, it detects outages in real time and sends instant alerts through multiple notification channels.",
+  authors: [
+    {
+      name: "Suhas Kanwar",
+      url: "https://github.com/SuhasKanwar/Uptiq"
+    },
+    {
+      name: "Suhas Kanwar",
+      url: "https://suhaskanwar.vercel.app"
+    }
+  ],
+  creator: "Suhas Kanwar",
+  keywords: [
+    "Website Monitoring",
+    "Uptime Monitoring",
+    "Service Availability",
+    "Real-time Alerts",
+    "Distributed Systems",
+    "Queue-Based Architecture",
+    "Worker Services",
+    "Notification System",
+    "Status Monitoring",
+    "Performance Tracking",
+    "Poller Services",
+    "Scalable Monitoring",
+    "Uptiq"
+  ]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className="h-full antialiased"
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
